41156cf9466ad544b5461e16b0924c5dd5a2ff27
mbacelar <mbacelar@gazetadopovo.com.br>
2023-02-09 12:32:01 -0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
+/* eslint-disable @typescript-eslint/camelcase */
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
   optimizeFonts: true,
   distDir: 'build',
+  serverMiddleware: [
+    createProxyMiddleware("/api", {
+      target: "https://maps.googleapis.com/maps/api/place/details/json",
+      changeOrigin: true,
+      pathRewrite: { "^/api": "" },
+    }),
+  ],
+};
 
-
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
